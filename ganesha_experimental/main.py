93328522e58a9079c9ce28aea425038a97ec1588
ellie hodjayev <zenmyx@gmail.com>
2022-07-20 11:49:01 +0000

    Update main.py

--- ganesha_experimental/main.py
+++ ganesha_experimental/main.py
@@ -2,6 +2,7 @@ import random
 
 
 def isPrime(n):
+    """Checks for Prime numbers"""
     # Corner case
     if n <= 1:
         return False
@@ -16,6 +17,7 @@ def isPrime(n):
 
 # Function to print primes
 def printPrime(n):
+    """Shows all Prime numbers before a given value printPrime(value)"""
     previous_primes_nb = []
     for i in range(2, n + 1):
         if isPrime(i):
@@ -24,12 +26,11 @@ def printPrime(n):
 
 
 def Prime():
+    """Will show all Prime numbers from a generated value (2, 100)"""
     n = random.randint(2, 100)
     Prime = str(printPrime(n))[1:-1]
     print(Prime)
 
 
-
 if __name__ == "__main__":
     Prime()
-
